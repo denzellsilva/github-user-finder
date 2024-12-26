5e4c4b49154a583dab36b5d5ecaf9ed7fa73e8a9
denzellsilva <silvamarldenzell@gmail.com>
2024-12-27 03:47:20 +0700

    made some changes

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // Octokit.js
 // https://github.com/octokit/core.js#readme
+import { Octokit } from "https://esm.sh/@octokit/core";
+
 const octokit = new Octokit({
-  auth: 'YOUR-TOKEN'
-})
+//  You can provide an authorization token to get more data, but since this project cannot hide a 
+//  auth: 'YOUR_TOKEN'
+});
 
 const response = await octokit.request('GET /users/{username}', {
   username: 'denzellsilva',
   headers: {
     'X-GitHub-Api-Version': '2022-11-28'
   }
-})
+});
 
 console.log(response);
