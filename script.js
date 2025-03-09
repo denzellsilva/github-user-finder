ba42a490c30943eae8eed3ec76d6b76fabb6168d
denzellsilva <silvamarldenzell@gmail.com>
2025-03-10 05:37:05 +0700

    decided to use fetch API and removed default form submission

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-// Octokit.js
-// https://github.com/octokit/core.js#readme
-import { Octokit } from "https://esm.sh/@octokit/core";
+const searchForm = document.querySelector('.search-box');
+const searchBtn = document.querySelector('.search-btn');
+const searchInput = document.querySelector('.search-box input');
 
-//  You can provide an authorization token to get more data
+// remove default form submission behavior
+searchForm.addEventListener('submit', e => e.preventDefault());
 
-// const octokit = new Octokit({
-//  auth: 'YOUR_API_KEY'
-// });
+searchBtn.addEventListener('click', () => {
+  const username =  searchInput.value;
+  const promise = fetchUserData(username);
 
-const octokit = new Octokit();
+  promise
+    .then(data => console.log(data))
+    .catch(error => console.error(`Can't find existing user: ${error}`));
+})
 
-async function octokitFetchData() {
-  const response = await octokit.request('GET /users/{username}', {
-    username: 'kamranahmedse',
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  });
-  
-  console.log(response);
+async function fetchUserData(username) {
+  let response;
+
+  if (username === '') {
+    response = await fetch('local-assets/kamranahmedse.json');
+  } else {
+    response = await fetch(`https://api.github.com/users/${username}`);
+  }
+
+  if (!response.ok) {
+    throw new Error(`HTTP error: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data;
 }
-
-octokitFetchData();
