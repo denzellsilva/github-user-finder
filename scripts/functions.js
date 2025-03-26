5db399eaa25cf2e7bdee26e405aca217807889ac
denzellsilva <silvamarldenzell@gmail.com>
2025-03-26 07:29:52 +0700

    Refactor: used && operator for conditionals in AdditionalInfo() instead of ternary operators

--- scripts/functions.js
+++ scripts/functions.js
@@ -76,29 +76,25 @@ function ProfileStats(data) {
 function AdditionalInfo(data) {
   return build(['ul'], 
   [
-    data['location'] ? build(['li'], 
+    data['location'] && build(['li'], 
     [
       `Location: ${data['location']}`
-    ])
-    : '',
+    ]),
     
-    data['company'] ? build(['li'], 
+    data['company'] && build(['li'], 
     [
       `Company: ${data['company']}`,
-    ])
-    : '',
+    ]),
 
-    data['blog'] ? build(['li'], 
+    data['blog'] && build(['li'], 
     [
       'Blog: ', build(['a', { href: data['blog'], target: 'blank' }], [data['blog']]),
-    ])
-    : '',
+    ]),
 
-    data['twitter_username'] ? build(['li'], 
+    data['twitter_username'] && build(['li'], 
     [
       'Twitter: ', build(['a', { href: `https://x.com/${data['twitter_username']}`, target: 'blank' }], [data['twitter_username'],]),
-    ])
-    : '',
+    ]),
   ]);
 }
 
