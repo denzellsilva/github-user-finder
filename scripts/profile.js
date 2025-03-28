340ecc9962a7fa197d76a4641244c1fc735a17f1
denzellsilva <silvamarldenzell@gmail.com>
2025-03-28 21:44:06 +0700

    changed fetchAllData() call to fetchAll()

--- scripts/profile.js
+++ scripts/profile.js
@@ -4,11 +4,12 @@ const searchForm = document.querySelector('.search-box');
 const searchBtn = document.querySelector('.search-btn');
 const searchInput = document.querySelector('.search-box input');
 const params = new URLSearchParams(window.location.search);
-const user = params.get('user');
-const userUrl = 'https://api.github.com/users/';
-const reposUrl = `https://api.github.com/search/repositories?q=user:${user}&sort=stars&order=desc`;
-// const userUrl = `../local-assets/kamranahmedse.json`;
-// const reposUrl = `../local-assets/kamranahmedse-repos.json`;
+// const user = params.get('user');
+// const userUrl = 'https://api.github.com/users/';
+// const reposUrl = `https://api.github.com/search/repositories?q=user:${user}&sort=stars&order=desc`;
+const user = `kamranahmedse.json`;
+const userUrl = `../local-assets/`;
+const reposUrl = `../local-assets/kamranahmedse-repos.json`;
 
 // whitelisting the url - redirect to '/' if the url doesn't have a 'user' parameter
 if (!params.has('user') || user === '') {
@@ -16,10 +17,10 @@ if (!params.has('user') || user === '') {
   window.location.href = '/';
 }
 
-functions.fetchAllData([fetch(userUrl + user), fetch(reposUrl)])
+functions.fetchAll([fetch(userUrl + user), fetch(reposUrl)])
   .then((data) => {
     functions.populate(data);
-    console.log(data);
+    // console.log(data);
   })
   .catch((e) => {
     functions.handleFetchError(e, () => {
@@ -27,7 +28,7 @@ functions.fetchAllData([fetch(userUrl + user), fetch(reposUrl)])
       window.location.href = '/';
     });
   });
-  
+
 // remove default form submission behavior
 searchForm.addEventListener('submit', e => e.preventDefault());
 
