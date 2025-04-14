b3640f90c2dbd684051c304d9db12ff54a9ee8b0
denzellsilva <silvamarldenzell@gmail.com>
2025-04-14 23:23:58 +0700

    fixed indentation

--- scripts/components/ProfileAdditionalInfo.js
+++ scripts/components/ProfileAdditionalInfo.js
@@ -1,28 +1,26 @@
 import { build } from '../build.js';
 
 export function ProfileAdditionalInfo(data) {
-	return build(['ul'], 
-	[
+  return build(['ul'], 
+  [
+    data['location'] && build(['li'], 
+    [
+      `Location: ${data['location']}`
+    ]),
 
-		data['location'] && build(['li'], 
-		[
-			`Location: ${data['location']}`
-		]),
+    data['company'] && build(['li'], 
+    [
+      `Company: ${data['company']}`,
+    ]),
 
-		data['company'] && build(['li'], 
-		[
-			`Company: ${data['company']}`,
-		]),
+    data['blog'] && build(['li'], 
+    [
+      'Blog: ', build(['a', { href: data['blog'], target: 'blank' }], [data['blog']]),
+    ]),
 
-		data['blog'] && build(['li'], 
-		[
-			'Blog: ', build(['a', { href: data['blog'], target: 'blank' }], [data['blog']]),
-		]),
-
-		data['twitter_username'] && build(['li'], 
-		[
-			'Twitter: ', build(['a', { href: `https://x.com/${data['twitter_username']}`, target: 'blank' }], [data['twitter_username'],]),
-		]),
-
-	]);
+    data['twitter_username'] && build(['li'], 
+    [
+      'Twitter: ', build(['a', { href: `https://x.com/${data['twitter_username']}`, target: 'blank' }], [data['twitter_username'],]),
+    ]),
+  ]);
 }
