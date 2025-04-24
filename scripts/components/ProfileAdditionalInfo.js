0835b4637829e047975d23c5c4442a8141819721
denzellsilva <silvamarldenzell@gmail.com>
2025-04-24 12:22:01 +0700

    implemented icons on profile additional info

--- scripts/components/ProfileAdditionalInfo.js
+++ scripts/components/ProfileAdditionalInfo.js
@@ -1,26 +1,31 @@
 import { build } from '../build.js';
+import { basePath } from '../functions.js';
 
 export function ProfileAdditionalInfo(data) {
-  return build(['ul'], 
+  return build(['ul', { class: 'additional-info' }], 
   [
     data['location'] && build(['li'], 
     [
-      `Location: ${data['location']}`
+      build(['img', { src: `${basePath('assets/location-icon.svg')}`, alt: 'Location: '}]),
+      build(['span'], [data['location']])
     ]),
 
     data['company'] && build(['li'], 
     [
-      `Company: ${data['company']}`,
+      build(['img', { src: `${basePath('assets/building-icon.svg')}`, alt: 'Company: '}]),
+      build(['span'], [data['company']])
     ]),
 
     data['blog'] && build(['li'], 
     [
-      'Blog: ', build(['a', { href: data['blog'], target: 'blank' }], [data['blog']]),
+      build(['img', { src: `${basePath('assets/link-icon.svg')}`, alt: 'Blog: '}]),
+      build(['a', { href: data['blog'], target: 'blank' }], [data['blog']]),
     ]),
 
     data['twitter_username'] && build(['li'], 
     [
-      'Twitter: ', build(['a', { href: `https://x.com/${data['twitter_username']}`, target: 'blank' }], [data['twitter_username'],]),
+      build(['img', { src: `${basePath('assets/twitter-icon.svg')}`, alt: 'Company: '}]),
+      build(['a', { href: `https://x.com/${data['twitter_username']}`, target: 'blank' }], [data['twitter_username']]),
     ]),
   ]);
 }
--- scripts/components/ProfileImage.js
+++ scripts/components/ProfileImage.js
@@ -1,5 +1,5 @@
 import { build } from '../build.js';
 
 export function ProfileImage(data) {
-  return build(['img', { src: data['avatar_url'] }]);
+  return build(['img', { src: data['avatar_url'], class: 'profile-img', alt: 'Profile Image' }]);
 }
