06c033222dbebac62cae6c7b4966e89fe0714720
denzellsilva <silvamarldenzell@gmail.com>
2025-04-24 14:50:33 +0700

    Refactor basePath() to allow putting or not putting a forward slash before specifying the path

--- scripts/functions.js
+++ scripts/functions.js
@@ -7,15 +7,31 @@ import { ProfileBio } from './components/ProfileBio.js';
 import { ProfileAdditionalInfo } from './components/ProfileAdditionalInfo.js';
 import { ReposSection } from './components/ReposSection.js';
 
-export function basePath(path) {
+export function basePath(path = '/') {
   const host = document.location.hostname;
+  let basePath = '/';
 
-  if (host === 'denzellsilva.github.io') {
-    const basePath = '/github-user-finder';
+  function getPath(basePath, path) {
+    if (path === '/') {
+      return basePath;
+    }
+    
+    // if the path starts with a slash, remove it and add it to the base path
+    if (path.startsWith('/')) {
+      path = path.slice(1);
+      return basePath + path;
+    }
+    
     return basePath + path;
   }
+  
+  // get the right path for the current host
+  if (host === 'denzellsilva.github.io') {
+    basePath = '/github-user-finder/';
+    return getPath(basePath, path);
+  }
 
-  return path;
+  return getPath(basePath, path);
 }
 
 export async function fetchUser(request) {
